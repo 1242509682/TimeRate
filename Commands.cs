10fcd8a9ede2f1183f487fe9a3ff2cd76e6365e8
羽学 <1242509682@qq.com>
2024-10-24 17:55:42 +0800

    update v1.1.0 加入了睡觉加速法
    v1.1.0
    美化了菜单
    优化了性能（不再发WorldInfo包）
    加入了视觉流畅优化配置（是否发TimeSet包）
    加入了睡觉也会提升加速的逻辑与相对开关指令

--- Commands.cs
+++ Commands.cs
@@ -7,18 +7,22 @@ internal class Commands
 {
     public static void times(CommandArgs args)
     {
-        var info = TimeRate.Config.Enabled ? "启用" : "禁用";
+        var info = TimeRate.Config.Enabled ? "[c/3ADB2C:启用]" : "[c/F25C71:关闭]";
+        var info2 = TimeRate.Config.All ? "[c/FFF261:启用]" : "[c/F25C71:关闭]";
+        var info3 = TimeRate.Config.One ? "[c/55A2E0:启用]" : "[c/F25C71:关闭]";
 
         if (args.Parameters.Count == 0)
         {
             if (args.Player != null)
             {
-                args.Player.SendMessage("【时间加速菜单】\n" +
-                    "/times —— 查看时间流速指令菜单\n" +
-                    "/times on 或 off —— 开启|关闭时间流速功能\n" +
-                    "/times set 数字 —— 设置时间流速(日晷为60 正常为1)\n" +
+                args.Player.SendMessage("[c/EBE9A2:时间加速插件] [C/BFDFEA:by] [c/00FFFF:羽学][i:3459]\n" +
+                    "/times —— 时间加速指令菜单\n" +
                     "/reload —— 重载配置文件\n" +
-                    $"服务器流速开关:[c/C9C7F5:{info}] 预设速率:[c/FFFCCF:{Terraria.Main.dayRate}]", Color.AntiqueWhite);
+                    "/times one —— 开启|关闭[c/55A2E0:单人睡觉]加速\n" +
+                    "/times all —— 开启|关闭[c/FFF261:全员睡觉]加速\n" +
+                    "/times on | off —— 开启|关闭[c/3ADB2C:指令]加速\n" +
+                    "/times set 数字 —— 设置速率|日晷:[c/F25C71:60] 正常:[c/55A2E0:1]", 235, 220, 199);
+                args.Player.SendMessage($"速率:[c/C9C7F5:{Terraria.Main.dayRate}] 指令:{info} 全员:{info2} 单人:{info3}", 247, 244, 150);
             }
             return;
         }
@@ -28,7 +32,7 @@ internal class Commands
             TimeRate.Config.Enabled = true;
             TimeRate.Config.Write();
             info = TimeRate.Config.Enabled ? "启用" : "禁用";
-            args.Player.SendSuccessMessage("已成功[c/C9C7F5:{0}]时间流速! 当前速率为：[c/FFFCCF:{1}]",info, TimeRate.Config.UpdateRate);
+            args.Player.SendSuccessMessage("已成功[c/C9C7F5:{0}]时间流速! 当前速率为：[c/FFFCCF:{1}]", info, TimeRate.Config.UpdateRate);
             return;
         }
 
@@ -38,6 +42,28 @@ internal class Commands
             TimeRate.Config.Write();
             info = TimeRate.Config.Enabled ? "启用" : "禁用";
             args.Player.SendSuccessMessage("已[c/C9C7F5:{0}]时间流速!当前速率为：[c/FFFCCF:{1}]!", info, Terraria.Main.dayRate);
+            return;
+        }
+
+        if (args.Parameters.Count == 1 && args.Parameters[0].ToLower() == "all")
+        {
+            var isSleep = TimeRate.Config.All;
+            TimeRate.Config.All = !isSleep;
+            TimeRate.Config.One = false;
+            TimeRate.Config.Write();
+            var Mess = isSleep ? "禁用" : "启用";
+            args.Player.SendSuccessMessage("已[c/C9C7F5:{0}]全员睡觉加速!当前速率为：[c/FFFCCF:{1}]!", Mess, Terraria.Main.dayRate);
+            return;
+        }
+
+        if (args.Parameters.Count == 1 && args.Parameters[0].ToLower() == "one")
+        {
+            var isSleep = TimeRate.Config.One;
+            TimeRate.Config.One = !isSleep;
+            TimeRate.Config.All = false;
+            TimeRate.Config.Write();
+            var Mess = isSleep ? "禁用" : "启用";
+            args.Player.SendSuccessMessage("已[c/C9C7F5:{0}]单人睡觉加速!当前速率为：[c/FFFCCF:{1}]!", Mess, Terraria.Main.dayRate);
             return;
         }
 
@@ -56,14 +82,6 @@ internal class Commands
                         }
                         break;
                     }
-                default:
-                    args.Player.SendInfoMessage("【时间流速菜单】\n" +
-                        "/times —— 查看时间流速指令菜单\n" +
-                        "/times on 或 off —— 开启|关闭时间流速功能\n" +
-                        "/times set 数字 —— 设置时间流速(附魔日晷速度是60 正常为1)\n" +
-                        "/reload —— 重载配置文件\n" +
-                        $"服务器流速开关:[c/C9C7F5:{info}] 速率:[c/FFFCCF:{TimeRate.Config.UpdateRate}]", Color.AntiqueWhite);
-                    return;
             }
         }
     }
